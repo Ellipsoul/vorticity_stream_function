434c508b7a0d1a4a4335b7c4c83c32219de99f00
Aron Teh <ewt17@ic.ac.uk>
2020-03-09 12:12:32 +0000

    Currently not working CMakeLists.txt file, but compiles on the command line

--- LidDrivenCavitySolver.cpp
+++ LidDrivenCavitySolver.cpp
@@ -4,6 +4,7 @@
 // External Libraries and Files
 #include "LidDrivenCavity.h"
 #include <boost/program_options.hpp>
+#include "cblas.h"
 
 namespace po = boost::program_options;
 
@@ -12,7 +13,7 @@ int main(int argc, char* argv[])
     // Create a new instance of the LidDrivenCavity class
     // Uses a pointer so solver is actually storing the address of the new instance
     LidDrivenCavity* solver = new LidDrivenCavity();
-    
+
     // Specify options that will be available to user
 
     // Option description
@@ -21,20 +22,20 @@ int main(int argc, char* argv[])
     );
     // Options (no default values)
     opts.add_options()
-        ("Lx", po::value<double>(),       "Length of the domain in the x-direction")
-        ("Ly", po::value<double>(),       "Length of the domain in the y-direction")
-        ("Nx", po::value<unsigned int>(), "Number of grid points in x-direction")
-        ("Ny", po::value<unsigned int>(), "Number of grid points in y-direction")
-        ("Px", po::value<unsigned int>(), "Number of partitions in the x-direction (parallel)")
-        ("Py", po::value<unsigned int>(), "Number of partitions in the y-direction (parallel)")
-        ("dt", po::value<double>(),        "Time step size")
-        ("T",  po::value<double>(),        "Final time")
-        ("Re", po::value<double>(),        "Reynolds number");
+        ("Lx", po::value<double>(),       "Length of the domain in the x-direction");
+        // ("Ly", po::value<double>(),       "Length of the domain in the y-direction")
+        // ("Nx", po::value<unsigned int>(), "Number of grid points in x-direction")
+        // ("Ny", po::value<unsigned int>(), "Number of grid points in y-direction")
+        // ("Px", po::value<unsigned int>(), "Number of partitions in the x-direction (parallel)")
+        // ("Py", po::value<unsigned int>(), "Number of partitions in the y-direction (parallel)")
+        // ("dt", po::value<double>(),        "Time step size")
+        // ("T",  po::value<double>(),        "Final time")
+        // ("Re", po::value<double>(),        "Reynolds number");
 
     // Parse command line options and generate map of options and values
-    //po::variables_map vm;
-    //po::store(po::parse_command_line(argc, argv, opts), vm);
-    //po::notify(vm);
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, opts), vm);
+    po::notify(vm);
 
     // Extract parameters into the proper data type
     //const double Lx = vm["Lx"].as<double>();
